554c42109d5373d4a3928cc8e920ee605f9690fc
Annie Pham <anniekpham@yahoo.com>
2019-06-08 11:03:53 -0700

    added react

---
+++ client/src/component/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = _ => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ client/src/pages/game/game.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from '../../component/header'
+
+class Game extends Component {
+    state = {
+        lastguess = null
+    }
+    
+    render () {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
+
+export default Game
