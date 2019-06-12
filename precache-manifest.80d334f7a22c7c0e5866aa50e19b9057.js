b95887e394df0bafc663829ec12bc8990ba34197
Annie Pham <anniekpham@yahoo.com>
2019-06-11 19:49:38 -0700

    Updates

---
+++ precache-manifest.80d334f7a22c7c0e5866aa50e19b9057.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81221bb7f76440d91c51e6d582ebc255",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "9d08b341690b1e33b22d",
+    "url": "/ClickyGame/static/css/main.14f51a21.chunk.css"
+  },
+  {
+    "revision": "98d488819882624c7ebb",
+    "url": "/ClickyGame/static/js/2.350a341d.chunk.js"
+  },
+  {
+    "revision": "9d08b341690b1e33b22d",
+    "url": "/ClickyGame/static/js/main.ff524eb2.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "b57d5732cecc292a5ef7c4dbbf676ec4",
+    "url": "/ClickyGame/static/media/300.b57d5732.png"
+  },
+  {
+    "revision": "8ef6e9e15167f5c793d4a7ff21a986ac",
+    "url": "/ClickyGame/static/media/303.8ef6e9e1.png"
+  },
+  {
+    "revision": "570802c7ac912de23fe74fd70c65efea",
+    "url": "/ClickyGame/static/media/501.570802c7.png"
+  },
+  {
+    "revision": "7805bc077da208cfe18d5786f040f01f",
+    "url": "/ClickyGame/static/media/502.7805bc07.png"
+  },
+  {
+    "revision": "34e45e8a99f25054d32dfc9fd0225181",
+    "url": "/ClickyGame/static/media/604.34e45e8a.png"
+  },
+  {
+    "revision": "fa74375cd4beebed89f99c021ca95c9d",
+    "url": "/ClickyGame/static/media/606.fa74375c.png"
+  },
+  {
+    "revision": "7b78ce8eac419801f8c6d8e7d75b04f7",
+    "url": "/ClickyGame/static/media/609.7b78ce8e.png"
+  },
+  {
+    "revision": "ddf552db8449d83a8b2cc9452260808e",
+    "url": "/ClickyGame/static/media/617.ddf552db.png"
+  },
+  {
+    "revision": "d45633feb693c027e93d76aa73241a81",
+    "url": "/ClickyGame/static/media/619.d45633fe.png"
+  },
+  {
+    "revision": "e3b7cf30c475356eadcbb1ebb6c49cb5",
+    "url": "/ClickyGame/static/media/624.e3b7cf30.png"
+  },
+  {
+    "revision": "69a7f528ed4fdcce4c9b7f4f97c2a8f3",
+    "url": "/ClickyGame/static/media/625.69a7f528.png"
+  },
+  {
+    "revision": "51886104b7e7d9030dcda673678f642d",
+    "url": "/ClickyGame/static/media/626.51886104.png"
+  }
+]);
